43e04e4bd3cafe9d952abaef64d2c48afcfa17d2
nafingatunngaliah <nafingatunngaliah@gmail.com>
2019-11-25 17:00:11 +0845

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
-    arg_parser.add_argument('-p', '--port', default=5000, help='port')
+    arg_parser.add_argument('-p', '--port', default=3000, help='port')
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
     options = arg_parser.parse_args()
 
