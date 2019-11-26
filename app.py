d4b651ab01310aa866823a07a2979669339c634f
nafingatunngaliah <nafingatunngaliah@gmail.com>
2019-11-26 14:39:57 +0845

    Update app.py

--- app.py
+++ app.py
@@ -70,13 +70,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     question = event.message.text
-    if(question == 'Zulfa cantik'):
-       line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Zulfa Jelek'))
-    else:
-        answer = request_api(question)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
-    #answer = request_api(question)
-    #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
+    #if(question == 'Zulfa cantik'):
+    #   line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Zulfa Jelek'))
+    #else:
+    #    answer = request_api(question)
+    #    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
+    answer = request_api(question)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
 
 def request_api(question):
     url = api_url + api_port + api_route
